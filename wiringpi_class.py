db4828c79799574dfa2cece05541d0da27b286ef
Philip Howard <phil@gadgetoid.com>
2012-07-09 16:13:44 +0100

    Added class-based wrapper

---
+++ wiringpi_class.py
@@ -0,0 +1,59 @@
+
+class Serial(object):
+    device = '/dev/ttyAMA0'
+    baud = 9600
+    serial_id = 0
+    def printf(self,*args):
+        return serialPrintf(self.serial_id,*args)
+    def dataAvail(self,*args):
+        return serialDataAvail(self.serial_id,*args)
+    def getchar(self,*args):
+        return serialGetchar(self.serial_id,*args)
+    def putchar(self,*args):
+        return serialPutchar(self.serial_id,*args)
+    def puts(self,*args):
+        return serialPuts(self.serial_id,*args)
+    def __init__(self,device,baud):
+        self.device = device
+        self.baud = baud
+        self.serial_id = serialOpen(self.device,self.baud)
+    def __del__(self):
+        serialClose(self.serial_id)
+
+class GPIO(object):
+    HIGH = 1
+    LOW = 0
+    MSBFIRST = 1
+    LSBFIRST = 0
+    WPI_MODE_PINS = 0
+    WPI_MODE_GPIO = 1
+    INPUT = 0
+    OUTPUT = 1
+    PWM_OUTPUT = 2
+    PUD_OFF = 0
+    PUD_DOWN = 1
+    PUD_UP = 2
+    def __init__(self):
+        wiringPiSetup()
+    def delay(self,*args):
+        delay(*args)
+    def delayMicroseconds(self,*args):
+        delayMicroseconds(*args)
+    def millis(self):
+        return millis()
+    def pinMode(self,*args):
+        pinMode(*args)
+    def gpioMode(self,*args):
+        wiringPiGpioMode(*args)
+    def digitalWrite(self,*args):
+        digitalWrite(*args)
+    def pwmWrite(self,*args):
+        pwmWrite(*args)
+    def digitalRead(self,*args):
+        return digitalRead(*args)
+    def shiftOut(self,*args):
+        shiftOut(*args)
+    def shiftOutWithDelay(self,*args):
+        shiftOutWithDelay(*args)
+    def shiftIn(self,*args):
+        return shiftIn(*args)
