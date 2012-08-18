c93dadf7cf826878968dc49048f3b7e6f6287691
lrvick <lance@lrvick.net>
2012-08-18 08:16:02 +0000

    migrate setup.py to setuptools and prepare for distribution

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
-wiringpi_module = Extension('_wiringpi',
-                            sources=['wiringpi_wrap.c', 'WiringPi/wiringPi/wiringPi.c', 'WiringPi/wiringPi/serial.c', 'WiringPi/wiringPi/wiringShift.c'],
-                            )
+wiringpi_module = Extension(
+    '_wiringpi',
+    sources=[
+        'wiringpi_wrap.c',
+        'WiringPi/wiringPi/wiringPi.c',
+        'WiringPi/wiringPi/serial.c',
+        'WiringPi/wiringPi/wiringShift.c'
+    ],
+)
 
-setup (name = 'wiringpi',
-        version = '1.0',
-        author = "Philip Howard",
-        description = """WiringPi for Python""",
-        ext_modules = [wiringpi_module],
-        py_modules = ["wiringpi"],
-        )
+setup(
+    name = 'wiringpi',
+    version = '1.0',
+    author = "Philip Howard",
+    packages = ["wiringpi"],
+    url = 'https://github.com/WiringPi/WiringPi-Python/',
+    description = """A python interface to WiringPi library which allows for
+    easily interfacing with the GPIO pins of the Raspberry Pi. Also supports
+    i2c and SPI""",
+    long_description=open('README.md').read(),
+    ext_modules = [wiringpi_module],
+    install_requires=[],
+)
