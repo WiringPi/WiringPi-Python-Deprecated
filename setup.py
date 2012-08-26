1dbebd309f30b8974eb52d8362cb91294a6db50a
Philip Howard <phil@gadgetoid.com>
2012-08-26 21:11:54 +0100

    Fixes to make working, buildable PyPi package

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Extension
+from setuptools import setup, find_packages, Extension
 
 wiringpi_module = Extension(
     '_wiringpi',
     sources=[
+        'WiringPi/wiringPi/lcd.c',
+        'WiringPi/wiringPi/piHiPri.c',
+        'WiringPi/wiringPi/piThread.c',
+        'WiringPi/wiringPi/wiringPiFace.c',
         'wiringpi_wrap.c',
         'WiringPi/wiringPi/wiringPi.c',
         'WiringPi/wiringPi/wiringSerial.c',
@@ -14,15 +18,21 @@ wiringpi_module = Extension(
 
 setup(
     name = 'wiringpi',
-    version = '1.0',
+    version = '1.0.5',
     author = "Philip Howard",
     author_email = "phil@gadgetoid.com",
     url = 'https://github.com/WiringPi/WiringPi-Python/',
     description = """A python interface to WiringPi library which allows for
     easily interfacing with the GPIO pins of the Raspberry Pi. Also supports
     i2c and SPI""",
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     ext_modules = [wiringpi_module],
     py_modules = ["wiringpi"],
     install_requires=[],
+    headers=[
+        'WiringPi/wiringPi/lcd.h',
+        'WiringPi/wiringPi/wiringPi.h',
+        'WiringPi/wiringPi/wiringSerial.h',
+        'WiringPi/wiringPi/wiringShift.h'
+        ]
 )
--- wiringpi.py
+++ wiringpi.py
@@ -150,3 +150,87 @@ serialPrintf = _wiringpi.serialPrintf
 # This file is compatible with both classic and new-style classes.
 
 
+HIGH = 1
+LOW = 0
+MSBFIRST = 1
+LSBFIRST = 0
+WPI_MODE_PINS = 0
+WPI_MODE_GPIO = 1
+WPI_MODE_SYS = 2
+MODE_PINS = 0
+MODE_GPIO = 1
+MODE_SYS = 2
+INPUT = 0
+OUTPUT = 1
+PWM_OUTPUT = 2
+PUD_OFF = 0
+PUD_DOWN = 1
+PUD_UP = 2
+
+class Serial(object):
+    device = '/dev/ttyAMA0'
+    baud = 9600
+    serial_id = 0
+    def printf(self,*args):
+        return serialPrintf(self.serial_id,*args)
+    def dataAvail(self,*args):
+        return serialDataAvail(self.serial_id,*args)
+    def getchar(self,*args):
+        return serialGetchar(self.serial_id,*args)
+    def putchar(self,*args):
+        return serialPutchar(self.serial_id,*args)
+    def puts(self,*args):
+        return serialPuts(self.serial_id,*args)
+    def __init__(self,device,baud):
+        self.device = device
+        self.baud = baud
+        self.serial_id = serialOpen(self.device,self.baud)
+    def __del__(self):
+        serialClose(self.serial_id)
+
+class GPIO(object):
+    HIGH = 1
+    LOW = 0
+    MSBFIRST = 1
+    LSBFIRST = 0
+    WPI_MODE_PINS = 0
+    WPI_MODE_GPIO = 1
+    WPI_MODE_SYS = 2
+    MODE_PINS = 0
+    MODE_GPIO = 1
+    MODE_SYS = 2
+    INPUT = 0
+    OUTPUT = 1
+    PWM_OUTPUT = 2
+    PUD_OFF = 0
+    PUD_DOWN = 1
+    PUD_UP = 2
+    MODE = 0
+    def __init__(self,pinmode=0):
+        self.MODE=pinmode
+        if pinmode==0:
+            wiringPiSetup()
+        elif pinmode==1:
+            wiringPiSetupGpio()
+        elif pinmode==2:
+            wiringPiSetupSys()
+    def delay(self,*args):
+        delay(*args)
+    def delayMicroseconds(self,*args):
+        delayMicroseconds(*args)
+    def millis(self):
+        return millis()
+    def pinMode(self,*args):
+        pinMode(*args)
+    def digitalWrite(self,*args):
+        digitalWrite(*args)
+    def pwmWrite(self,*args):
+        pwmWrite(*args)
+    def digitalRead(self,*args):
+        return digitalRead(*args)
+    def shiftOut(self,*args):
+        shiftOut(*args)
+    def shiftOutWithDelay(self,*args):
+        shiftOutWithDelay(*args)
+    def shiftIn(self,*args):
+        return shiftIn(*args)
